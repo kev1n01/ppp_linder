3019d762ce517f5f6932e88e131a93b3e4f02ee4
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-16 17:18:16 -0500

    feat: make model and migration to employee

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+      'emp_num_doc',
+      'emp_birthdate',
+      'emp_email',
+      'emp_address',
+      'emp_status',
+      'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_07_16_171207_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('emp_num_doc');
+            $table->date('emp_birthdate')->nullable();
+            $table->string('emp_email', 70)->nullable();
+            $table->string('emp_address', 100)->nullable();
+            $table->boolean('emp_status')->default(true);
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
