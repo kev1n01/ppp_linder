7b2e9f631d61acb661599b986e8ab7d064e0ee10
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-17 15:52:59 -0500

    feat: list record by emplyee id or all to admin

--- app/Filament/Resources/SaleResource/Pages/ListSales.php
+++ app/Filament/Resources/SaleResource/Pages/ListSales.php
@@ -5,10 +5,25 @@ namespace App\Filament\Resources\SaleResource\Pages;
 use App\Filament\Resources\SaleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-
+use Illuminate\Database\Eloquent\Builder;
 class ListSales extends ListRecords
 {
     protected static string $resource = SaleResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        $query = parent::getTableQuery();
+
+        $user = auth()->user();
+
+        if ($user->hasRole('empleado')) {
+            // Filtrar por empleado_id asociado al usuario
+            $query->where('employee_id', $user->employee->id ?? null);
+        }
+
+        // Si el usuario es admin, no filtramos (muestra todo)
+        return $query;
+    }
 
     protected function getHeaderActions(): array
     {
