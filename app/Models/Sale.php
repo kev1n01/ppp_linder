c7609856cac26d91d351f246c31d9385e87892e0
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-17 00:49:50 -0500

    change name relationship

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -24,7 +24,7 @@ class Sale extends Model
         return $this->belongsTo(Employee::class);
     }
     
-    public function saledetail()
+    public function sale_details()
     {
         return $this->hasMany(SaleDetail::class);
     }
