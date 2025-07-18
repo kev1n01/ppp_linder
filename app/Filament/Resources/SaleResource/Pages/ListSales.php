b7ed2200701af6414eaf3cb30fefedeba68f0767
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-18 00:44:33 -0500

    filtrando registros cuando es usuario empleado solo devolver ventas por su id

--- app/Filament/Resources/SaleResource/Pages/ListSales.php
+++ app/Filament/Resources/SaleResource/Pages/ListSales.php
@@ -5,10 +5,21 @@ namespace App\Filament\Resources\SaleResource\Pages;
 use App\Filament\Resources\SaleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListSales extends ListRecords
 {
     protected static string $resource = SaleResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        $user = auth()->user();
+        if ($user->hasRole('empleado')) {
+          return static::getResource()::getEloquentQuery()->where('employee_id',  $user->employee->id);
+        }else{
+          return static::getResource()::getEloquentQuery();
+        }
+    }
 
     protected function getHeaderActions(): array
     {
