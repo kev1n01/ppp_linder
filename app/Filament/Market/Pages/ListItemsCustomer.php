cc72397d1d024d462c1d82bcc3dad0db89a8d0ba
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-21 08:19:42 -0500

    moviendo a pages istitemscustomer para ver en modo admin de filament

---
+++ app/Filament/Market/Pages/ListItemsCustomer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Market\Pages;
+
+use App\Models\Item;
+use Filament\Pages\Page;
+
+class ListItemsCustomer extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-bolt';
+
+    protected static string $view = 'filament.pages.list-items-customer';
+
+    protected static ?string $navigationLabel = 'Productos y servicios';  
+
+    protected static ?string $title = ''; 
+    
+    protected static ?string $slug = 'productos-y-servicio';
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()->hasRole('cliente');
+    }
+}
