ef6d9ecbe6a1b64e2f0395302a810f4f85f30709
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-23 19:35:34 -0500

    feat: propuesta para mamejo de stock al actualizar venta

--- app/Filament/Resources/SaleResource/Pages/EditSale.php
+++ app/Filament/Resources/SaleResource/Pages/EditSale.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\SaleResource\Pages;
 
 use App\Filament\Resources\SaleResource;
+// use App\Models\SaleDetail;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -16,4 +17,48 @@ class EditSale extends EditRecord
             // Actions\DeleteAction::make(),
         ];
     }
+    
+  //   protected function beforeSave(): void
+  //   {
+  //   dd($this->record, $this->record->saleDetails);
+
+  //   $venta = $this->record;
+
+  //   // Obtener los detalles antiguos desde BD
+  //   $originalDetails = SaleDetail::where('sale_id', $venta->id)->get()->keyBy('id');
+
+  //   foreach ($venta->saleDetails as $detail) {
+  //       $original = $originalDetails->get($detail->id);
+
+  //       if (!$detail->item || $detail->item->ite_type !== 'producto') {
+  //           continue;
+  //       }
+
+  //       $item = $detail->item;
+
+  //       // 1. Si existe original, resta el original y aplica el nuevo
+  //       if ($original) {
+  //           $cantidadOriginal = $original->sald_quantity;
+  //           $cantidadNueva = $detail->sald_quantity;
+
+  //           $diferencia = $cantidadOriginal - $cantidadNueva;
+
+  //           // Si diferencia > 0, se devuelve stock. Si < 0, se descuenta más.
+  //           $item->increment('ite_stock', $diferencia);
+  //       } else {
+  //           // 2. Si es un nuevo detalle, descuenta stock
+  //           $item->decrement('ite_stock', $detail->sald_quantity);
+  //       }
+  //   }
+
+  //   // 3. Detectar si se eliminaron detalles
+  //   $nuevosIds = collect($venta->saleDetails)->pluck('id')->filter()->all();
+  //   $eliminados = $originalDetails->except($nuevosIds);
+
+  //   foreach ($eliminados as $eliminado) {
+  //       if ($eliminado->item && $eliminado->item->ite_type === 'producto') {
+  //           $eliminado->item->increment('ite_stock', $eliminado->sald_quantity);
+  //       }
+  //   }
+  // }
 }
