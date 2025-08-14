afc4a4b26c1f7711a70420effd1a9fb48e96d792
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-08-13 22:30:06 -0500

    borrando config de greenter para eliminar conflictos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Invoices\InvoiceController;
 use App\Http\Controllers\MarketController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -9,24 +8,6 @@ use Inertia\Inertia;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/market/list', [MarketController::class, 'index'])->name('market.index');
-
-// Route::get('/home', function () {
-//     return redirect('login');
-// })->name('home');
-
-// Route::get('dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//   Route::get('reminders', function () {
-//     return Inertia::render('Reminder');
-//   })->name('reminders');
-
-//   Route::controller(InvoiceController::class)->group(function () {
-//     Route::get('/invoices', 'index')->name('invoices.index');
-//   });
-// });
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
